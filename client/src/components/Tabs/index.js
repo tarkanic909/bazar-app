a28f9f5a4d6a3821bfa06436920274cdcd96e600
tarkanic909 <tarkanic909@gmail.com>
2021-03-04 10:14:44 +0100

    added carousel in main page

--- client/src/components/Tabs/index.js
+++ client/src/components/Tabs/index.js
@@ -1,15 +1,28 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
 import { ProductsContext } from "../../contexts/ProductsContext";
+import ProductItem from "../../shared/ProductItem";
+import SliderNav from "../../shared/SliderNav";
 import "./index.scss";
 import TabsItem from "./TabsItem";
 
 function Tabs() {
   const [tab, setTab] = useState("must_have");
-  const [, setItems] = useState([]);
+  const [items, setItems] = useState([]);
+  const [slider, setSlider] = useState();
   const { products } = useContext(ProductsContext);
+  const slidesToShow = window.innerWidth >= "768" ? 4 : 1;
 
   function changeTab(tab) {
     setTab(tab);
+  }
+
+  function handelPrevClick() {
+    slider.slickPrev();
+  }
+
+  function handleNextclick() {
+    slider.slickNext();
   }
 
   useEffect(() => {
@@ -45,7 +58,28 @@ function Tabs() {
             changeTab("special_offers");
           }}
         />
+        <div className="tabs__slider-nav">
+          <SliderNav clickNext={handleNextclick} clickPrev={handelPrevClick} />
+        </div>
       </ul>
+      <div className="tabs__slider">
+        <Slider
+          ref={(slider) => {
+            setSlider(slider);
+          }}
+          // autoplay={true}
+          arrows={false}
+          pauseOnHover={false}
+          slidesToShow={slidesToShow}
+          slidesToScroll={1}
+        >
+          {items.map((item) => (
+            <div className="tabs__slide">
+              <ProductItem product={item} />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 }
