3a428b7945252a0fb47a977ce6443776fa4293b8
tarkanic909 <tarkanic909@gmail.com>
2021-03-09 08:19:23 +0100

    added orange variant

--- client/src/shared/Button/index.js
+++ client/src/shared/Button/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./index.scss";
 
 function Button(props) {
+  const orange = props.orange ? " button--orange" : "";
   return (
-    <button {...props} className="button">
+    <button {...props} className={`button ${orange}`}>
       {props.children}
     </button>
   );
